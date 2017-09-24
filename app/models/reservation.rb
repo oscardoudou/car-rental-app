43b416149d7508c92d9657b5804d095054185b8f
Chengcheng Shao <cshao@ncsu.edu>
2017-09-24 15:40:03 -0400

    optimize the time check

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,17 @@
 class Reservation < ApplicationRecord
   #this code need to be changed, one user can only reserve one car one time
   #has_many :line_items, :dependent => :destroy
-  #has_many :line_items, :dependent => :destroy
+=begin
+  validates :after_current
+
+  def after_current
+    return if :checkout_time.blank?
+
+    if :checkout_time < :reserve_time
+      errors.add(:checkout_time, "must be after the start date")
+    end
+  end
+=end
 
 =begin
   def add_car(car_id)
