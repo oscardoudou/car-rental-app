d447e14407a2b6f6caa08d8b40d89fbd63199943
feiteng <tfei3@ncsu.edu>
2017-09-25 14:55:04 -0400

    can create admin and super admin now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin, :sadmin)
   end
 
   def logged_in_user
