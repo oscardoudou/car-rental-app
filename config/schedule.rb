5dbd61572461fa13df0100a00df00200e07c9466
Chengcheng Shao <cshao@ncsu.edu>
2017-09-25 11:25:37 -0400

    add whenever task

---
+++ config/schedule.rb
@@ -0,0 +1,24 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :environment, :development
+set :output, "log/cron_log.log"
+
+every 1.minute do
+  # puts "cd-------------#############测试"
+   rake "cancel_reservation_30mins"
+end
+
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+
+
+
--- lib/tasks/monitor_reservation.rake
+++ lib/tasks/monitor_reservation.rake
@@ -1,7 +1,13 @@
 task :cancel_reservation_30mins => :environment do
+
+  begin
+    Car.joins("LEFT JOIN reservations on cars.id = reservations.car_id").where("reservations.checkout_time < ? and reservations.status = ?", (Time.now-30.minutes).to_s, 'reserved').update_all(:status => 'available')
+  end
+
   begin
     Reservation.where("checkout_time < ? and status = ?", (Time.now-30.minutes).to_s, 'reserved').update_all(:status => 'cancled')
   end
+
 
 end
 
