61272a816d29c56748547021478c859cad01c86e
oscardoudou <oscar9591@icloud.com>
2017-09-26 23:40:11 -0400

    unable confirming reservation with existed order

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,6 +6,8 @@ class Reservation < ApplicationRecord
 
   validate :have_reservation?
 
+  validate :have_order?
+
   def after_current?
     return if checkout_time.blank?
     if checkout_time < reserve_time
@@ -14,10 +16,17 @@ class Reservation < ApplicationRecord
   end
 
   def have_reservation?
-    #check_reservation = Reservation.where(:user_id => self.user_id, :status => ["reserved", "checkedout"])
+  #  #check_reservation = Reservation.where(:user_id => self.user_id, :status => ["reserved", "checkedout"])
     check_reservation = Reservation.find_by_user_id_and_status(self.user_id, ["reserved", "checkedout"])
     if !check_reservation.nil?
       errors.add(:id, "you have unfinished reservation")
+    end
+  end
+
+  def have_order?
+    check_order = Order.find_by_user_id_and_status(self.user_id,["checkedout"])
+    unless check_order.nil?
+      errors.add(:id, "you have unfinished order")
     end
   end
 
