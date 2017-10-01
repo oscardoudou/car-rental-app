18bbc3d1ab2dc39a3272dcb8752fae10a377d059
Chengcheng Shao <cshao@ncsu.edu>
2017-10-01 14:59:50 -0400

    Revert "destroy reservation: delete related order"
    This reverts commit 7772bd30e926b2184dfa43225e19a32032d9a19e.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,40 +1,33 @@
 class Reservation < ApplicationRecord
 
-  has_one :order, dependent: :destroy
-
   validates :user_id, presence: true
 
-  validate :after_current?, :on => :create
+  validate :after_current?
 
-  validate :have_reservation?, :on => :create
+  validate :have_reservation?
 
-#  validate :have_related_order?, :on => :destroy
-
-#  before_destroy :have_related_order?
+  validate :have_order?
 
   def after_current?
     return if checkout_time.blank?
     if checkout_time < reserve_time
-      errors.add(:base, "You must be after the current time")
+      errors.add(:checkout_time, "must be after the current time")
     end
   end
 
   def have_reservation?
-    #  #check_reservation = Reservation.where(:user_id => self.user_id, :status => ["reserved", "checkedout"])
+  #  #check_reservation = Reservation.where(:user_id => self.user_id, :status => ["reserved", "checkedout"])
     check_reservation = Reservation.find_by_user_id_and_status(self.user_id, ["reserved", "checkedout"])
     if !check_reservation.nil?
-      errors.add(:base, "You have unfinished reservation")
+      errors.add(:id, "you have unfinished reservation")
     end
   end
 
-  #def have_related_order?
-    # check_order = Order.find_by_reservation_id(self.id)
-    # if !check_order.nil?
-      #if (check_order.reservation_id.eql ==self.id)
-        #errors.add(:base, "You have related order, you should destroy the order first")
-        #return false
-      #end
-    # end
-  #end
+  def have_order?
+    check_order = Order.find_by_user_id_and_status(self.user_id,["checkedout"])
+    unless check_order.nil?
+      errors.add(:id, "you have unfinished order")
+    end
+  end
 
 end
