018c87eebc68ea048c28a9b0f603947bf25a8085
Chengcheng Shao <cshao@ncsu.edu>
2017-10-07 11:39:35 -0400

    optimize db migrate

--- db/migrate/20170917123250_create_orders.rb
+++ db/migrate/20170917123250_create_orders.rb
@@ -2,22 +2,24 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
 
-
+      t.integer :reservation_id
+      t.integer :car_id
+      t.integer :user_id
 
       t.string :name
+      t.integer :tel
       t.text :address
       t.string :email
       t.string :pay_type
-      t.integer :car_id
-      t.integer :user_id
-      t.integer :reservation_id
-      t.integer :tel
+
       t.string :status
-      t.timestamp :real_return_time
-      t.timestamp :real_checkout_time
+
+      t.timestamp :checkout_time
       t.timestamp :return_time
+      t.timestamp :real_checkout_time
+      t.timestamp :real_return_time
 
-
+      t.decimal :charge, :precision=>8, :scale=>2
 
       t.timestamps
     end
--- db/migrate/20171004003631_add_charge_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddChargeToOrders < ActiveRecord::Migration[5.1]
-  def change
-    add_column :orders, :charge, :decimal, precision: 8, scale: 2
-  end
-end
