1cda2a9a907a508239e08c893294145e370038b6
oscardoudou <oscar9591@icloud.com>
2017-10-07 18:20:01 -0400

    test for model cars finished

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -4,4 +4,22 @@ class CarTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+   test  "license_plate length should be 7 "do
+     car   = Car.new()
+     car.license_plate = 'XX0000'
+     assert_not car.save
+   end
+
+   test  "license_plate length should be unique "do
+     car = Car.new()
+     car.license_plate = 'XX00001'
+     assert_not car.save
+   end
+
+   test  "car with 7 bit length and unqie license_plate should be saved "do
+     car = Car.new()
+     car.license_plate = 'XX00003'
+     assert car.save
+   end
 end
